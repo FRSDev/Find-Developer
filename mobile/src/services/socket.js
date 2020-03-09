33a0153e639d821b2c580e9473e4038548fe29de
FRSDev <santoz.rodrigues@hotmail.com>
2020-03-09 19:57:47 -0300

    mobileCommit
    Mobile

---
+++ mobile/src/services/socket.js
@@ -0,0 +1,33 @@
+import socketio from 'socket.io-client'
+
+const socket = socketio('http://192.168.0.108:3333', { autoConnect: false })
+
+
+function subscribeToNewDevs(subscribeFucntion ) {
+    socket.on('new-dedv', subscribeFucntion)
+}
+
+function connect(latitude, longitude, techs){
+    socket.io.opts.query = {
+        latitude,
+        longitude,
+        techs
+    }
+    socket.connect()
+
+    socket.on('message', text => {
+        console.log(text)
+    })
+}
+
+function disconnect (){
+    if(socket.connected){
+        socket.disconnect()
+    }
+}
+
+export {
+    connect,
+    disconnect,
+    subscribeToNewDevs,
+}
